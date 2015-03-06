4aeb530478d5090908baa8b4b24e749806d0ebbe
IoFisho <mquade93@gmail.com>
2015-03-06 17:55:20 +0100

    v0.13Fisho

--- Assets/CubeScripts/rotate.cs
+++ Assets/CubeScripts/rotate.cs
@@ -10,6 +10,6 @@ public class rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (0, 100 * Time.deltaTime, 0);
+		transform.Rotate (1, 100 * Time.deltaTime, 0);
 	}
 }
